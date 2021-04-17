768a857032fc65180b424b449c9257bfa0e0d18c
github Toshimatu <matsuda.akt@gmail.com>
2021-04-17 21:48:19 +0900

    [Add] コンポーネントのグローバル登録とローカル登録について

---
+++ src/components/LikeNumber.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>いいね{{ number }}</p>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      number: 5,
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
