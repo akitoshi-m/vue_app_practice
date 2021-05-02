8f4719cc1c179719b1a8d7c8f9012d8b172428e1
nexceed <matsuda.a@nexceed.com>
2021-05-02 21:39:53 +0900

    [Add] クエリの指定

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -10,7 +10,7 @@
     <router-link to="/users/2">User2へ</router-link>
     <router-link :to="'/users/' + (Number(id) + 1) + '/profile'">次のユーザー</router-link>
     <br>
-    <router-link :to="{ name: 'users-id-profile', params: { id: Number(id) + 1 }}">次のユーザー(nameで指定)</router-link>
+    <router-link :to="{ name: 'users-id-profile', params: { id: Number(id) + 1 }, query: { lang: 'ja', page: 2} }">次のユーザー(nameで指定)</router-link>
     <router-view></router-view>
   </div>
 </template>
